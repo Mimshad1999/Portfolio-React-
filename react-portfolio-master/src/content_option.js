ffb6a7942dbcf35db861b4b2061eddd7c3be3495
MIMSHAD ALAM <119530266+Mimshad1999@users.noreply.github.com>
2024-07-11 17:46:41 +0530

    first commit

---
+++ react-portfolio-master/src/content_option.js
@@ -0,0 +1,167 @@
+// import cat from "../src/assets/images/cat.jpg";
+const logotext = "Mimshad";
+const meta = {
+    title: "mimshad Alam ",
+    description: "i'm Mimshad Alam working as internship",
+};
+
+const introdata = {
+    title: "I’m Mimshad Alam",
+    animated: {
+        first: "I love coding",
+        second: "I code cool websites",
+        third: "I developed Machine Learning Projects",
+    },
+    // description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at nisl euismod urna bibendum",
+    your_img_url: "https://img.freepik.com/premium-photo/portrait-successful-programmer-game-developer-coder-guy-uses-computer-laptop-work-game-design-hacker-boy-generative-ai-cyber-gamer_117038-7605.jpg",
+};
+
+const dataabout = {
+    title: "about my self",
+    aboutme: "Highly motivated and detail-oriented Computer Science graduate seeking an entry-level position in data analysis or softwareengineering to leverage my skills and internship experience for the benefit of the organization.",
+};
+const worktimeline = [{
+        jobtitle: "Software Internship ",
+        where: "Salahkaar Consultant",
+        date: "2024",
+    },
+    // {
+    //     jobtitle: "Designer of week",
+    //     where: "Jamalya",
+    //     date: "2019",
+    // },
+    // {
+    //     jobtitle: "Designer of week",
+    //     where: "ALquds",
+    //     date: "2019",
+    // },
+];
+
+const skills = [{
+        name: "Python",
+        value: 90,
+    },
+    {
+        name: "Database",
+        value: 85,
+    },
+    {
+        name: "Javascript",
+        value: 80,
+    },
+    {
+        name: "React",
+        value: 60,
+    },
+    {
+        name: "Jquery",
+        value: 85,
+    },
+];
+
+const services = [{
+        title: "UI & UX Design",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at nisl euismod urna bibendum sollicitudin.",
+    },
+    {
+        title: "Mobile Apps",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at nisl euismod urna bibendum sollicitudin.",
+    },
+    {
+        title: "Wordpress Design",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at nisl euismod urna bibendum sollicitudin.",
+    },
+];
+
+const dataportfolio = [{
+        img: "https://s3-eu-west-1.amazonaws.com/discovery-assets-production/app/uploads/2020/03/27214321/cropped-dc169f53-27e1-4d6f-bab4-ad1444d5d6e9-CATvsDOG_S1_TT-1310x737.jpg",
+        description: "Cat vs dogs classification using deep learning .",
+        link: "https://github.com/Mimshad1999/Cat-vs-dogs-classification",
+    },
+    {
+        img: "https://cdn-images-1.medium.com/max/750/1*IC1b2AnebaOYNK8PdZFwTQ.jpeg",
+        description: "This project analyzes the Titanic dataset to gain insights into survival factors during the Titanic disaster",
+        link: "https://github.com/Mimshad1999/Titanic_Survival_prediction",
+    },
+    // {
+    //     img: "https://picsum.photos/400/?grayscale",
+    //     description: "The wisdom of life consists in the elimination of non-essentials.",
+    //     link: "#",
+    // },
+    // {
+    //     img: "https://picsum.photos/400/600/?grayscale",
+    //     description: "The wisdom of life consists in the elimination of non-essentials.",
+    //     link: "#",
+    // },
+    // {
+    //     img: "https://picsum.photos/400/300/?grayscale",
+    //     description: "The wisdom of life consists in the elimination of non-essentials.",
+    //     link: "#",
+    // },
+    // {
+    //     img: "https://picsum.photos/400/700/?grayscale",
+    //     description: "The wisdom of life consists in the elimination of non-essentials.",
+    //     link: "#",
+    // },
+
+    // {
+    //     img: "https://picsum.photos/400/600/?grayscale",
+    //     description: "The wisdom of life consists in the elimination of non-essentials.",
+    //     link: "#",
+    // },
+    // {
+    //     img: "https://picsum.photos/400/300/?grayscale",
+    //     description: "The wisdom of life consists in the elimination of non-essentials.",
+    //     link: "#",
+    // },
+     {
+        img: "https://www.healthworkscollective.com/wp-content/uploads/2021/03/shutterstock_1512490073-scaled.jpg",
+        description: "The Doctor Recommmendation System based on their diseas.",
+        link: "https://www.healthworkscollective.com/wp-content/uploads/2021/03/shutterstock_1512490073-scaled.jpg",
+    },
+    {
+        img: "https://www.posteezy.com/sites/default/files/styles/olivero_hero/public/2023-11/NKrHsv62KDuB5OM9WRfuRb2N23xozvmoUtQs0rYy.jpg?itok=Ed2RI4Qz",
+        description: "Crop recoommendation System using Machine Learning.",
+        link: "https://github.com/Mimshad1999/crop_recommendation",
+    },
+    {
+        img: "https://th.bing.com/th/id/OIP.CsJSZUXF1XUgYxXJ8wcNdAHaE5?rs=1&pid=ImgDetMain",
+        description: "Airlines-Data-Analysis-using-SQL-and-Python",
+        link: "https://github.com/Mimshad1999/Data-Analysis-",
+    },
+    // {
+    //     img: "https://picsum.photos/400/700/?grayscale",
+    //     description: "The wisdom of life consists in the elimination of non-essentials.",
+    //     link: "#",
+    // },
+];
+
+const contactConfig = {
+    YOUR_EMAIL: "mimshadalam1999@gmail.com",
+    YOUR_FONE: "7903666575",
+    description: "Highly motivated and detail-oriented Computer Science graduate seeking an entry-level position in Software developer and im working as Software developer intern in salakhar consultant. ",
+    // creat an emailjs.com account 
+    // check out this tutorial https://www.emailjs.com/docs/examples/reactjs/
+    YOUR_SERVICE_ID: "service_id",
+    YOUR_TEMPLATE_ID: "template_id",
+    YOUR_USER_ID: "user_id",
+};
+
+const socialprofils = {
+    github: "https://github.com/Mimshad1999",
+    facebook: "https://www.facebook.com/mimshad.alam.58?mibextid=ZbWKwL",
+    linkedin: "https://www.linkedin.com/in/mimshad-alam-995a20230/",
+    // twitter: "https://twitter.com",
+};
+export {
+    meta,
+    dataabout,
+    dataportfolio,
+    worktimeline,
+    skills,
+    services,
+    introdata,
+    contactConfig,
+    socialprofils,
+    logotext,
+};
